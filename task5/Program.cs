1a7a155f3067e978154d288fd0e31881bed6daf0
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-07-31 17:04:06 +0530

    Add project files.

---
+++ task5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    public class Program
+    {
+        public enum shapes
+        {
+            circle,
+            rectangle,
+            square
+        }
+        public static void areaCalculation(int number1, shapes structure,params object[] number2)
+        {
+            Console.WriteLine("Enum AreaCalculation:");
+            if ((int) structure == 0) 
+            {
+                Console.WriteLine("Area of circle:"+3.14*number1*number1);
+            }
+           else if ((int) structure == 2)
+            {
+                Console.WriteLine("Area of square:" + number1 * number1);
+            }
+            else if ((int)structure == 1)
+            {
+                foreach (int i in number2)
+                Console.WriteLine("Area of rectangle:"+ number1*i);
+            }
+
+        }
+        public static void iteration(int number1, params object[] number2)
+        {
+            foreach (Enum shape in Enum.GetValues(typeof(shapes))) 
+            {
+                if (shape.ToString() == "circle")
+                {
+                    Console.WriteLine("Area of circle:" + 3.14 * number1 * number1);
+                }
+                else if (shape.ToString() == "rectangle")
+                {
+                    foreach (int i in number2)
+                        Console.WriteLine("Area of rectangle:" + number1 * i);
+                }
+                else if(shape.ToString() =="square")
+                {
+                    Console.WriteLine("Area of square:" + number1 * number1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            //areaCalculation( 10,shapes.circle);
+            //areaCalculation(10,  shapes.square);
+            //areaCalculation(10, shapes.rectangle, 11);
+           
+
+            iteration(11,21);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task5/TaxCalculation.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task5;
+
+namespace task5
+{
+
+    public abstract class details
+    {
+        public abstract string Name { get; set; }
+        public abstract double Salary { get; set; }
+        protected double salary;
+        protected double tax;
+        protected string name;
+        public abstract void calculateTax();
+
+
+    }
+    public class taxPercentage1 : details
+    {
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+        public override void calculateTax()
+        {
+
+            tax = 15 * salary / 100;
+            Console.WriteLine($"Tax:{tax}"); 
+        }
+    }
+    public class taxPercentage2 : details
+    {
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+        public override void calculateTax()
+        {
+
+            tax = 16 * salary / 100;
+            Console.WriteLine($"Tax:{tax}");
+        }
+    }
+    public class taxPercentage3 : details
+    {
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+        public override void calculateTax()
+        {
+
+             
+            Console.WriteLine($"Tax:No tax ");
+        }
+    }
+
+    public class TaxCalculation
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name1=Console.ReadLine();
+            Console.Write("Salary:");
+            double salary1=Convert.ToDouble(Console.ReadLine());
+            if (salary1 > 10000)
+            {
+                taxPercentage1 tax = new taxPercentage1();
+                tax.Name = name1;
+                tax.Salary = salary1;
+                tax.calculateTax();
+            }
+            else if (salary1 > 20000)
+            {
+                taxPercentage2 tax = new taxPercentage2();
+                tax.Name = name1;
+                tax.Salary = salary1;
+                tax.calculateTax();
+            }
+            else
+            {
+                taxPercentage3 tax = new taxPercentage3();
+                tax.Name = name1;
+                tax.Salary = salary1;
+                tax.calculateTax();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
